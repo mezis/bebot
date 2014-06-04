6293f78e945ee42bc2d081aaae7cb9c1ea91cc81
Julien Letessier <julien.letessier@gmail.com>
2014-06-04 11:23:12 +0200

    Adds schedule for Slack notifications

--- scheduler.rb
+++ scheduler.rb
@@ -15,7 +15,15 @@ COMPARISONS = JSON.parse(ENV['BEBOT_COMPARISONS'])
 COMPARISONS.each do |payload|
   scheduler.every '5m', first_in:'1s' do |job|
     Bebot::Services::CompareBranches.new(
-      repo: payload['repo'], from: payload['from'], to: payload['to']
+      repo: payload['repo'], from: payload['from'], to: payload['to'],
+      notify: %w(datadog log)
+    ).run
+  end
+  
+  scheduler.every '1h', first_at: Rufus::Scheduler.parse('9:05am Europe/London') do |job|
+    Bebot::Services::CompareBranches.new(
+      repo: payload['repo'], from: payload['from'], to: payload['to'],
+      notify: %w(log slack)
     ).run
   end
 end
